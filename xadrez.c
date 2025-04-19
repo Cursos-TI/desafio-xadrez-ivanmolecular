c32b98e11fe0e7bb035f6c2a34050dee748b6f8b
Ivan Martin Zborowski <161751855+ivanmolecular@users.noreply.github.com>
2025-04-19 02:35:15 +0000

    Atualizado desafio: laco de repeticao nivel avancado

--- xadrez.c
+++ xadrez.c
@@ -3,7 +3,6 @@
 int main() {
     // Movimento da TORRE usando FOR
     // A Torre vai se mover 5 casas para a direita
-    // Alterado o incremento usado em i para i1
     int i1;
     printf("Movimento da Torre:\n");
     for (i1 = 1; i1 <= 5; i1++) {
@@ -14,7 +13,6 @@ int main() {
 
     // Movimento do BISPO usando WHILE
     // O Bispo vai se mover 5 casas na diagonal para cima e à direita
-    // Alterado o incremento usado em i para i2
     int i2 = 1;
     printf("Movimento do Bispo:\n");
     while (i2 <= 5) {
@@ -26,13 +24,31 @@ int main() {
 
     // Movimento da RAINHA usando DO-WHILE
     // A Rainha vai se mover 8 casas para a esquerda
-    // Alterado o incremento usado em i para i3
     int i3 = 1;
     printf("Movimento da Rainha:\n");
     do {
         printf("Movimento %d - Esquerda\n", i3);
         i3++;
     } while (i3 <= 8);
+
+    printf("\n"); // Separador visual
+
+    // Movimento do CAVALO usando FOR + WHILE
+    // O Cavalo se move 2 casas para baixo e 1 para a esquerda (em L)
+    printf("Movimento do Cavalo:\n");
+
+    for (int m = 1; m <= 1; m++) {
+        int passo = 1;
+
+        // Duas casas para baixo
+        while (passo <= 2) {
+            printf("Movimento %d - Baixo\n", passo);
+            passo++;
+        }
+
+        // Uma casa para a esquerda
+        printf("Movimento 3 - Esquerda\n");
+    }
 
     return 0;
 }
